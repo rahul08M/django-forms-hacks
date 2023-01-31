80583ea73e3ac46e093990eaaad07a55af7f1d67
Rahul Mohite <41115652+rahul08M@users.noreply.github.com>
2023-01-31 19:39:01 +0530

    Update form_select2.py

--- select2/form_select2.py
+++ select2/form_select2.py
@@ -1,1 +1,20 @@
+from django import forms
 
+
+class BaseSelect2Form(forms.Form):
+
+    RANDOM_CHOICES = (
+        ('Naruto', 'Naruto'),
+        ('Kiba', 'Kiba'),
+        ('Kakashi', 'Kakashi'),
+        ('Obito', 'Obito'),
+        ('Rin', 'Rin'),
+    )
+
+    multiple_choices = forms.MultipleChoiceField(choices=RANDOM_CHOICES)
+
+    def __init__(self, *args, **kwargs):
+        super(BaseSelect2Form, self).__init__(*args, **kwargs)
+
+        # adding html attributes
+        self.fields['multiple_choices'].widget.attrs['class'] = 'select2'
