a6083a96c04746b7d0483c555e577ca3d62ddd49
Rahul Mohite <41115652+rahul08M@users.noreply.github.com>
2023-02-09 15:13:41 +0530

    Update forms.py

--- simple_captcha/forms.py
+++ simple_captcha/forms.py
@@ -1,1 +1,28 @@
+from django import forms
+from captcha.fields import CaptchaField
 
+
+class BaseCaptchaForm(forms.Form):
+
+    first_name = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                'class': 'css-class',
+                'placeholder': 'First Name'
+            }
+        )
+    )
+
+    last_name = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                'class': 'css-class',
+                'placeholder': 'Last Name'
+            }
+        )
+    )
+
+    captcha = CaptchaField()  # django simple captcha's captcha field
+
+    def __init__(self, *args, **kwargs):
+        super(BaseCaptchaForm, self).__init__(*args, **kwargs)
